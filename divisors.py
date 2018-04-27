7aff8c217f036e6a046fd862024a85eafec6cd65
brunsond <37097298+brunsond@users.noreply.github.com>
2018-04-26 20:42:23 -0600

    Create divisors.py

---
+++ divisors.py
@@ -0,0 +1,14 @@
+#Prints out list of the divisors of a positive integer.
+
+n = float(input('Enter integer >= 1. '))
+divisors = [1]
+for i in range(2,int(n**0.5)+1):
+    if (n%i==0):
+        if (i!=n/i):
+            divisors.append(i)
+            divisors.append(int(n/i))
+        else:
+            divisors.append(i)
+divisors.append(int(n))
+divisors.sort()
+print(divisors)
