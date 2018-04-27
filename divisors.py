3b304566ce794ced1afc52450d855fd6adc6a84f
brunsond <37097298+brunsond@users.noreply.github.com>
2018-04-26 20:46:44 -0600

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,5 +1,7 @@
 #Prints out list of the divisors of a positive integer.
 
+# Written by Douglas Brunson (2018).
+
 n = float(input('Enter integer >= 1. '))
 divisors = [1]
 for i in range(2,int(n**0.5)+1):
